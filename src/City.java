1d38d81511ca64a5c33df75f0909303f74e4576e
liondy <michaelliondy05@gmail.com>
2019-10-23 21:35:34 +0700

    Initial Project

---
+++ src/City.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author michael
+ */
+public class City {
+    public int x;
+    public int y;
+    
+    public City(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author michael
+ */
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+//        while(sc.hasNext()){
+//            int n = sc.nextInt();
+//            int x = sc.nextInt();
+//            int y = sc.nextInt();
+//        }
+        int x;
+        for (int i = 1; i <= 10; i++) {
+//            x = r.nextInt(20);
+            System.out.println(Math.random());
+        }
+    }
+    
+}
