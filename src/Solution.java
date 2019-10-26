eac6a799e76399fafed47a01b9e5034d7045a0b4
liondy <michaelliondy05@gmail.com>
2019-10-26 18:20:00 +0700

    bug overwrite all solutions :(

--- src/Solution.java
+++ src/Solution.java
@@ -13,19 +13,33 @@ public class Solution {
     double[] fitness;
     
     public Solution(CityPopulation cityPopulation, double[][] distanceMap){
-        this.populations = new CityPopulation[cityPopulation.totalCity];
-        this.fitness = new double[cityPopulation.totalCity];
-        for (int i = 0; i < cityPopulation.totalCity; i++) {
-            CityPopulation newCityPopulation = cityPopulation;
+        this.populations = new CityPopulation[cityPopulation.totalCity+1];
+        this.fitness = new double[cityPopulation.totalCity+1];
+        CityPopulation temp = cityPopulation;
+        CityPopulation[] tmp = new CityPopulation[cityPopulation.totalCity+1];
+        for (int i = 1; i <= cityPopulation.totalCity; i++) {
+            CityPopulation newCityPopulation = new CityPopulation();
+            newCityPopulation = temp;
             newCityPopulation.countTotalCity();
             newCityPopulation.shuffle();
             newCityPopulation.setDistanceMap(distanceMap);
             newCityPopulation.calcFitness();
-            this.addNewSolution(i,newCityPopulation);
+            System.out.println(i);
+            tmp[i] = newCityPopulation;
+            this.populations[i] = tmp[i];
+//            this.addNewSolution(i,newCityPopulation);
+            temp.cities = newCityPopulation.cities;
+//            this.printAllSolution();
+//            System.out.print("Solution "+ i + ": " );
+//            for (int j = 1; j <= this.populations[i].totalCity; j++) {
+//                System.out.print(this.populations[i].getCities(j).getNumber()+" ");
+//            }
+//            System.out.println();
         }
     }
     
     public void addNewSolution(int idx, CityPopulation sol){
+        System.out.println(idx);
         this.populations[idx] = sol;
     }
     
@@ -34,9 +48,13 @@ public class Solution {
     }
     
     public void printAllSolution(){
-        for (int i = 1; i <= this.populations.length; i++) {
-            for (int j = 0; j < this.populations[i].totalCity; j++) {
-                System.out.println(this.populations[i].cities.get(j)+" ");
+        System.out.println();
+        for (int i = 1; i < this.populations.length; i++) {
+            if(this.populations[i]!=null){
+                System.out.print("Solution "+ i + ": " );
+                for (int j = 1; j <= this.populations[i].totalCity; j++) {
+                    System.out.print(this.populations[i].getCities(i).getNumber()+" ");
+                }
             }
             System.out.println();
         }
