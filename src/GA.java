c904e40636472dddfc7e9ecabd40e1dcbbd165d3
Michael Liondy <liondy@ftis.unpar>
2019-11-04 13:20:39 +0700

    debug

--- src/GA.java
+++ src/GA.java
@@ -13,8 +13,18 @@ public class GA {
     public static RoutePopulation evolve(RoutePopulation population){
         RoutePopulation nextGeneration = new RoutePopulation(population.getPopulationSize());
         for (int i = 0; i < nextGeneration.getPopulationSize(); i++) {
+            System.out.println();
             Route parent1 = selection(population);
             Route parent2 = selection(population);
+            System.out.println("parent1: ");
+            for (int j = 0; j < parent1.getTotalCity(); j++) {
+                System.out.print(parent1.getRoute().get(j).getNumber()+" ");
+            }
+            System.out.println();
+            System.out.println("parent2: ");
+            for (int j = 0; j < parent2.getTotalCity(); j++) {
+                System.out.print(parent2.getRoute().get(j).getNumber()+" ");
+            }
             Route[] child = crossover(parent1,parent2);
         }
         return nextGeneration;
@@ -22,7 +32,9 @@ public class GA {
     
     public static Route selection(RoutePopulation population){
         double selection = Math.random();
+        System.out.println("selection: "+selection);
         int idx = binarySearch(population,0,population.getPopulationSize()-1,selection);
+        System.out.println("idx: "+idx);
         if(idx!=-1){
             Route selected = population.getPopulation()[idx];
             return selected;
@@ -31,7 +43,7 @@ public class GA {
     }
     
     private static int binarySearch(RoutePopulation population, int left, int right, double selection){
-        if(right>=left){
+        if(right>left){
             int idx = (left+right)/2;
             if(population.getPopulation()[idx].getCumulative()>=selection && selection<= population.getPopulation()[idx+1].getCumulative()){
                 return idx;
--- src/Main.java
+++ src/Main.java
@@ -50,6 +50,5 @@ public class Main {
         routePopulation.countPeluangKumulatif();
         routePopulation.printAllSolution();
         routePopulation = GA.evolve(routePopulation);
-        
     }
 }
