d439b2455729ce195e6f4a8e7b5eb808244e6f76
Michael Liondy <liondy@ftis.unpar>
2019-11-11 09:18:25 +0700

    add source code documentation

--- src/Main.java
+++ src/Main.java
@@ -56,30 +56,65 @@ public class Main {
         routePopulation.setTotalFitness(totalFitness);
         routePopulation.countPeluangKumulatif();
 //        routePopulation.printAllSolution();
-        Route bestRoute = firstRoute;
+
+        //get the (initial = best) route, best fitness (max fitness), and best distance (min distance), and before first generation (generation = 0)
+        ArrayList<City> bestRoute = (ArrayList<City>) firstRoute.getRoute().clone();
+        double bestFitness = firstRoute.getFitness();
+        double bestDistance = firstRoute.getDistance();
         int generation = 0;
+        
+        //first evolution
         routePopulation = GA.evolve(routePopulation);
-        Route firstGenerationBest = routePopulation.getFittest();
-//        System.out.println("Solution 1: "+firstGenerationBest.getFitness());
-        if(firstGenerationBest.getFitness()>bestRoute.getFitness()){
-            bestRoute = firstGenerationBest;
+        
+        //get the first best route, best fitness of first generation (max fitness of first generation), and best distance of first generation (min distance of first generation)
+        Route firstBest = routePopulation.getFittest();
+        ArrayList<City> firstGenerationBest = (ArrayList<City>) firstBest.getRoute().clone();
+        double firstGenerationBestFitness = firstBest.getFitness();
+        double firstGenerationBestDistance = firstBest.getDistance();
+        System.out.println("Solution 1: "+firstGenerationBestFitness);
+        
+        //check if the first generation best fitness (max fitness of first generation) is better than our assumption on initial route fitness
+        if(firstGenerationBestFitness>bestFitness){
+            bestRoute = (ArrayList<City>) firstGenerationBest.clone();
+            bestFitness = firstGenerationBestFitness;
+            bestDistance = firstGenerationBestDistance;
             generation = 1;
         }
+        
+        //loop through 100 generation to find the best route for this TSP
         for (int i = 2; i <= 100; i++) {
+            //first of all, evolve the population of current generation
             routePopulation = GA.evolve(routePopulation);
+            
+            //get the best route of each generation, best fitness of each generation (max fitness of each generation), and best distance of each generation (min distance of each generation)
             Route best = routePopulation.getFittest();
-//            System.out.println("Solution "+i+": "+best.getFitness());
-            if(best.getFitness()>bestRoute.getFitness()){
-                bestRoute = best;
+            ArrayList<City> bestGenerationNow = (ArrayList<City>) best.getRoute().clone();
+            double bestFitnessNow = best.getFitness();
+            double bestDistanceNow = best.getDistance();
+            System.out.println("Solution "+i+": "+bestFitnessNow);
+            
+            //check if the current generation best fitness (max fitness of current generation) is better than our assumption of best route before
+            if(bestFitnessNow>bestFitness){
+                bestRoute = (ArrayList<City>) bestGenerationNow.clone();
+                bestFitness = bestFitnessNow;
+                bestDistance = bestDistanceNow;
                 generation = i;
             }
         }
+        
+        //Output the best solution after that 100 generation
         System.out.println("After 100 generation, best route is on generation "+ generation+" with order: ");
-        for (int i = 0; i < bestRoute.getTotalCity(); i++) {
-            System.out.print(bestRoute.getRoute().get(i).getNumber()+" ");
+        
+        //Print the order of best route
+        for (int i = 0; i < bestRoute.size(); i++) {
+            System.out.print(bestRoute.get(i).getNumber()+" ");
         }
         System.out.println();
-        System.out.println("Fitness: "+bestRoute.getFitness());
-        System.out.println("Distance: "+bestRoute.getDistance());
+        
+        //Print the fitness of that best route
+        System.out.println("Fitness: "+bestFitness);
+        
+        //Print the distance of that best route
+        System.out.println("Distance: "+bestDistance);
     }
 }
